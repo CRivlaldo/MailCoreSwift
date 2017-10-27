ef492e0f494066329c4bcb7b8619818bd23fe643
Vladimir Vlasov <crivlaldo@yandex.ru>
2017-10-27 14:11:08 +0300

    Initial commit

---
+++ MailCoreSwift/MailCoreSwift.h
@@ -0,0 +1,19 @@
+//
+//  MailCoreSwift.h
+//  MailCoreSwift
+//
+//  Created by Vladimir Vlasov on 27/10/2017.
+//  Copyright © 2017 MobilityLab, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MailCoreSwift.
+FOUNDATION_EXPORT double MailCoreSwiftVersionNumber;
+
+//! Project version string for MailCoreSwift.
+FOUNDATION_EXPORT const unsigned char MailCoreSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MailCoreSwift/PublicHeader.h>
+
+#import <MailCore/MailCore.h>
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'MailCoreSwift' do
+  pod 'mailcore2-ios'
+end
